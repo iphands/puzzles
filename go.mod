121b37718b38b65586c77bdc0a956db42a663d51
Ian Page Hands <iphands@gmail.com>
2022-03-09 10:31:40 -0500

    go impl rev-add-two-nums

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module example.com/m/v2
 
 go 1.17
+
+require github.com/nsf/gocode v0.0.0-20190302080247-5bee97b48836 // indirect
--- src/rev-add-two-nums/main.go
+++ src/rev-add-two-nums/main.go
@@ -1,7 +1,111 @@
 package main
 
-import "fmt"
- 
+import (
+	"fmt"
+	"math/big"
+)
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	listToInt := func(l *ListNode) *big.Int {
+		str := ""
+		for true {
+			str = fmt.Sprintf("%s%d", str, l.Val)
+
+			if l.Next == nil {
+				break
+			}
+
+			l = l.Next
+		}
+
+		str = reverseStr(str)
+
+		num, ok := new(big.Int).SetString(str, 10)
+		if !ok {
+			panic("toobig")
+		}
+
+		return num
+	}
+
+	num := big.NewInt(0)
+	num.Add(listToInt(l1), listToInt(l2))
+	numStr := fmt.Sprintf("%d", num)
+	numStrLen := len(numStr) - 1
+
+	head := &ListNode{}
+	curr := head
+	for i, c := range reverseStr(numStr) {
+		curr.Val = int(c - 48)
+		if i == numStrLen {
+			break
+		}
+		curr.Next = &ListNode{}
+		curr = curr.Next
+	}
+
+	return head
+}
+
+func getData(arr []int) *ListNode {
+	head := &ListNode{}
+	curr := head
+
+	for i, item := range arr {
+		curr.Val = item
+		if i == len(arr)-1 {
+			break
+		}
+
+		curr.Next = &ListNode{}
+		curr = curr.Next
+	}
+
+	return head
+}
+
+func reverse(arr []int) []int {
+	ret := []int{}
+	for i := len(arr) - 1; i > -1; i-- {
+		ret = append(ret, arr[i])
+	}
+	return ret
+}
+
+func reverseStr(str string) string {
+	arr := []rune(str)
+	ret := []rune{}
+	for i := len(arr) - 1; i > -1; i-- {
+		ret = append(ret, arr[i])
+	}
+	return string(ret)
+}
+
+func printNodes(head *ListNode) {
+	fmt.Printf("%v %v\n", head.Val, head)
+	if head.Next == nil {
+		return
+	}
+
+	printNodes(head.Next)
+}
+
 func main() {
 	fmt.Println("Hello")
+	// printNodes(getData([]int{1, 2, 3}))
+	l1 := getData([]int{2, 4, 9})
+	l2 := getData([]int{5, 6, 4, 9})
+	printNodes(addTwoNumbers(l1, l2))
+
+	// [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]
+	// [5,6,4]
+
+	l1 = getData([]int{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1})
+	l2 = getData([]int{5, 6, 4})
+	printNodes(addTwoNumbers(l1, l2))
 }
