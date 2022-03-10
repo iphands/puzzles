63ca1013e18499beca2c319ab9ff9f5ab0f3fb9d
Ian Page Hands <iphands@gmail.com>
2022-03-10 09:03:18 -0500

    adding next-permutation/main.py

---
+++ src/next-permutation/main.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+
+class Solution(object):
+    @staticmethod
+    def isLargest(lst):
+        curr = lst[0]
+        for i, num in enumerate(lst):
+            if i == 0: continue
+            if lst[i-1] < num:
+                return False
+        return True
+
+    @staticmethod
+    def findLesser(lst):
+        last = lst[-1]
+        for i, num in reversed(list(enumerate(lst))):
+            if last > num:
+                return i
+            last = num
+        assert(False)
+
+    @staticmethod
+    def findNextLargest(lst):
+        ret = (1000000000, 0) # this leading number is naughty
+        for i, num in enumerate(lst):
+            # print("DEBUG: lst:{} ret:{} num:{}".format(lst, ret, num))
+            if num > lst[0] and num <= ret[0]:
+                ret = (num, i)
+        return ret[1]
+
+    @staticmethod
+    def swap(lst, s1, s2):
+        tmp = lst.copy()
+        lst[s1] = tmp[s2]
+        lst[s2] = tmp[s1]
+
+    @staticmethod
+    def reverse(lst, start):
+        sub_list = lst[start+1:]
+        if len(sub_list) < 2:
+            # print("DEBUG0: lst:{} sub:{} start:{}".format(lst, sub_list, start))
+            return
+
+        # print("DEBUG1: lst:{} sub{}:".format(lst, sub_list))
+        sub_list.reverse()
+        # print("DEBUG2: lst:{} sub{}:".format(lst, sub_list))
+        for i, num in enumerate(sub_list):
+            lst[i + start + 1] = num
+        # print("DEBUG3: lst:{} sub{}:".format(lst, sub_list))
+
+    def nextPermutation(self, nums):
+        if self.isLargest(nums):
+            return nums.sort()
+
+        swap1 = self.findLesser(nums)
+        swap2 = self.findNextLargest(nums[swap1:]) + swap1
+        # print("DEBUG: {} {}".format(swap1, swap2))
+        self.swap(nums, swap1, swap2)
+        self.reverse(nums, swap1)
+
+        return None
+
+#######################
+
+def test(l, ans):
+    res = l.copy()
+    Solution().nextPermutation(res)
+    if res != ans:
+        print("Fail: {} != {} for {}".format(res, ans, l))
+        assert(False)
+        return
+    print("Pass: {} == {} for {}".format(res, ans, l))
+
+
+test([1,2,3], [1,3,2])
+test([2,3,1], [3,1,2])
+test([1,1,5], [1,5,1])
+test([3,2,1], [1,2,3])
+test([1,2,3,4], [1,2,4,3])
+test([1,2], [2,1])
+test([1,3,2], [2,1,3])
+test([3,1,2], [3,2,1])
+test([2,3,1,3,3], [2,3,3,1,3])
