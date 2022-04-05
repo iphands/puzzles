b7c16e5f8b81752f36efd4920628b5b66a31986b
Ian Page Hands <iphands@gmail.com>
2022-04-04 22:16:38 -0400

    longest-palindromic-substring: merged two check funcs and added memoization

--- src/longest-palindromic-substring/main.py
+++ src/longest-palindromic-substring/main.py
@@ -2,46 +2,13 @@ from ..common.utils import log, check
 import csv
 import cProfile
 
+memo = {}
+
 class Solution:
-    # Warn this is super hacktastic brute force! :D
-    def longestPalindromeOld(self, s: str) -> str:
-        length = len(s)
-        if length < 2:
-            return s
-
-        #  check if the whole thing is a palindrome
-        if self.check_string(s, length):
-           return s
-
-        for size in reversed(range(length)):
-            if size == 1:
-                return s[0]
-
-            for i in range(length):
-                sizei = size + i
-                if sizei > length:
-                    break
-
-                if s[i] != s[sizei-1] or s[i+1] != s[sizei-2]: # fast pre-flight check
-                    continue
-
-                if self.check_string(s[i:sizei], size):
-                    return s[i:sizei]
-
-        return "foo"
-
-    def check_string(self, s: str, size: int) -> bool:
-        middle = size//2
-        part1  = s[0:middle]
-        part2  = s[middle:][::-1]
-
-        if size%2:
-            part1  = s[0:middle+1]
-
-        return part1 == part2
 
     def longestPalindrome(self, s: str) -> str:
         s_len = len(s)
+        s_len_short = s_len - 1
         if s_len < 2:
             return s
 
@@ -50,38 +17,16 @@ class Solution:
             return s[0]
 
         #  check if the whole thing is a palindrome
-        if self.check_string(s, s_len):
-           return s
+        if s == s[::-1]:
+            return s
 
-        def check_odd(start):
+        def check(start, is_even):
+            l = start
             r = start
-            l = start
+            if is_even:
+                r = start + 1
 
-            ret = s[0]
-            for i in range(1, s_len):
-                l -= 1
-                if l < 0:
-                    l += 1
-                    break
-
-                r += 1
-                if r > s_len - 1:
-                    r -= 1
-                    break
-
-                # log("- start:{} r:{} l:{} s[l]:{} s[r]:{}".format(start, r, l, s[l], s[r]))
-                if s[l] == s[r]:
-                    ret = s[l:r+1]
-                    continue
-                break
-
-            return ret
-
-        def check_even(start):
-            r = start + 1
-            l = start
-
-            if r > s_len-1 or s[r] != s[l]:
+            if r > s_len_short or s[r] != s[l]:
                 return s[0]
 
             ret = s[0]
@@ -96,7 +41,7 @@ class Solution:
                     break
 
                 r += 1
-                if r > s_len - 1:
+                if r > s_len_short:
                     r -= 1
                     break
 
@@ -111,16 +56,22 @@ class Solution:
 
         ret = s[0]
         for i in range(s_len):
+            if (s, i) in memo:
+                ret = memo[(s,i)]
+                continue
+
+            ret_len = len(ret)
             # log("checking odd  from i:{} s[i]:{}".format(i, s[i]))
-            result = check_odd(i)
-            if len(ret) < len(result):
+            result = check(i, False)
+            if ret_len < len(result):
                 ret = result
 
             # log("checking even from i:{} s[i]:{}".format(i, s[i]))
-            result = check_even(i)
-            if len(ret) < len(result):
+            result = check(i, True)
+            if ret_len < len(result):
                 ret = result
 
+            memo[s,i] = ret
 
         return ret
 
@@ -145,8 +96,15 @@ def do_all_tests():
         reader = csv.reader(data)
         for row in reader:
             test(row[0].strip(), row[1].strip())
+            test(row[0].strip(), row[1].strip())
+            test(row[0].strip(), row[1].strip())
+            test(row[0].strip(), row[1].strip())
+            test(row[0].strip(), row[1].strip())
+            test(row[0].strip(), row[1].strip())
+            test(row[0].strip(), row[1].strip())
+            test(row[0].strip(), row[1].strip())
 
 if __name__ == '__main__':
-    for i in range(10):
-        # cProfile.run('do_all_tests()')
-        do_all_tests()
+    for i in range(1):
+        cProfile.run('do_all_tests()')
+        # do_all_tests()
